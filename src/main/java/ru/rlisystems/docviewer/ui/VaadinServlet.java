e8ea295785f4c66e30264c41993f186c41a10d68
Dmitriy Balakin <dmitriy.balakin.82@gmail.com>
2014-11-30 15:43:44 +0300

    Версия 1.0

--- src/main/java/ru/rlisystems/docviewer/ui/VaadinServlet.java
+++ src/main/java/ru/rlisystems/docviewer/ui/VaadinServlet.java
@@ -10,7 +10,7 @@ import static com.vaadin.server.Constants.SERVLET_PARAMETER_UI_PROVIDER;
 @WebServlet (value = "/*",
 			 asyncSupported = true,
 			 initParams = {
-				 @WebInitParam (name = SERVLET_PARAMETER_PRODUCTION_MODE, value = "false"),
+				 @WebInitParam (name = SERVLET_PARAMETER_PRODUCTION_MODE, value = "true"),
 				 @WebInitParam (name = PARAMETER_WIDGETSET, value = "ru.rlisystems.docviewer.ui.DocumentViewer"),
 				 @WebInitParam (name = SERVLET_PARAMETER_UI_PROVIDER, value = "com.vaadin.cdi.CDIUIProvider")
 })
